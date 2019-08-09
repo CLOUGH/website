14e4db8a77c90dafaebe8f187bf43acb7bd79516
Warren <clough.warren@gmail.com>
2019-08-09 00:13:32 -0500

    remove test and remove error in test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { MaintenancePageComponent } from './maintenance-page/maintenance-page.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent,
-        MaintenancePageComponent
+        AppComponent
       ],
       imports: [
         AppRoutingModule

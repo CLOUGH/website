5779894dd5fac9be7cd03ea9b49fea94e2554ff4
Warren <clough.warren@gmail.com>
2019-10-03 08:02:44 -0500

    fix function expression

--- src/app/store/link/link.reducer.ts
+++ src/app/store/link/link.reducer.ts
@@ -2,10 +2,10 @@ import { LinkActions, ELinkActions } from './link.action';
 import { initialLinkState, ILinkState } from './link.state';
 
 
-export const linkReducers = (
+export function linkReducers(
   state = initialLinkState,
   action: LinkActions
-): ILinkState => {
+): ILinkState {
   console.log({type: action.type});
   switch (action.type) {
     case ELinkActions.GetLinksSuccess: {
